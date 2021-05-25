7b957f15fa0c49ff57c09228739f149b6d16d930
ebzeal <ebenezerajayi@gmail.com>
2021-05-25 05:41:58 +0100

    Using Cascading Inserts and Updates

--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -4,12 +4,15 @@ import { Coffee } from './entities/coffee.entity';
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { Flavour } from './entities/flavour.entity';
 
 @Injectable()
 export class CoffeesService {
   constructor(
     @InjectRepository(Coffee)
     private readonly coffeeRepository: Repository<Coffee>,
+    @InjectRepository(Flavour)
+    private readonly flavourRepository: Repository<Flavour>,
   ) {}
 
   findAll() {
@@ -28,15 +31,27 @@ export class CoffeesService {
     return coffee;
   }
 
-  create(createCoffeeDto: CreateCoffeeDto) {
-    const coffee = this.coffeeRepository.create(createCoffeeDto);
+  async create(createCoffeeDto: CreateCoffeeDto) {
+    const flavours = await Promise.all(
+      createCoffeeDto.flavours.map((name) => this.preloadFlavourByName(name)),
+    );
+    const coffee = this.coffeeRepository.create({
+      ...createCoffeeDto,
+      flavours,
+    });
     return this.coffeeRepository.save(coffee);
   }
 
   async update(id: string, updateCoffeeDto: UpdateCoffeeDto) {
+    const flavours =
+      updateCoffeeDto.flavours &&
+      (await Promise.all(
+        updateCoffeeDto.flavours.map((name) => this.preloadFlavourByName(name)),
+      ));
     const coffee = await this.coffeeRepository.preload({
       id: +id,
       ...updateCoffeeDto,
+      flavours,
     });
     if (!coffee) {
       throw new NotFoundException(`Coffee #${id} not found`);
@@ -47,5 +62,13 @@ export class CoffeesService {
   async remove(id: string) {
     const coffee = await this.findOne(id);
     return this.coffeeRepository.remove(coffee);
+  }
+
+  private async preloadFlavourByName(name: string): Promise<Flavour> {
+    const existingFlavour = await this.flavourRepository.findOne({ name });
+    if (existingFlavour) {
+      return existingFlavour;
+    }
+    return this.flavourRepository.create({ name });
   }
 }
