26e73ba7bd059af9b5ef8fec4f0ef5a965068960
ebzeal <ebenezerajayi@gmail.com>
2021-05-27 05:26:29 +0100

    Factory Providers useFactory

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -2,27 +2,31 @@ import { COFFEE_BRANDS } from './coffees.constants';
 import { Event } from './../events/entities/event.entity';
 import { CoffeesService } from './coffees.service';
 import { CoffeesController } from './coffees.controller';
-import { Module } from '@nestjs/common';
+import { Module, Injectable } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Coffee } from './entities/coffee.entity';
 import { Flavour } from './entities/flavour.entity';
 
-class ConfigService {}
-class DevelopmentConfigService {}
-class ProductionConfigService {}
+@Injectable()
+export class CoffeeBrandsFactory {
+  create() {
+    // do something
+    return ['Black chocolat', 'nescafe'];
+  }
+}
+
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavour, Event])],
   controllers: [CoffeesController],
   providers: [
     CoffeesService,
+    CoffeeBrandsFactory,
     {
-      provide: ConfigService,
-      useClass:
-        process.env.NODE_ENV === 'development'
-          ? DevelopmentConfigService
-          : ProductionConfigService,
+      provide: COFFEE_BRANDS,
+      useFactory: (brandsFactory: CoffeeBrandsFactory) =>
+        brandsFactory.create(),
+      inject: [CoffeeBrandsFactory],
     },
-    { provide: COFFEE_BRANDS, useValue: ['Black chocolat', 'nescafe'] },
   ],
   exports: [CoffeesService],
 })
